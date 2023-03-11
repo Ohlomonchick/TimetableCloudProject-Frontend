a212e050852c6745559776bca00ce11bebaefc28
Ohlomonchick <proskurin.dima16@gmail.com>
2023-03-11 11:38:55 +0300

    profile info

--- src/views/MyAccount.vue
+++ src/views/MyAccount.vue
@@ -2,7 +2,11 @@
   <div class="page-my-account">
       <div class="columns is-multiline">
           <div class="column is-12">
-              <h1 class="title">My account</h1>
+              <h1 class="title">Account of {{ user.username }}</h1>
+            <h2 class="title is-grey-dark">My groups: </h2>
+            <div v-for="group in user.participant_groups" class="box">
+              <h3 class=" is-dark">{{ group.name }}</h3>
+          </div>
           </div>
 
           <div class="column is-12">
@@ -21,6 +25,20 @@ import axios from "axios";
 
 export default {
   name: "MyAccount",
+  data() {
+    return {
+      user_id: 0,
+      user: {},
+      base_url: '',
+    }
+  },
+  beforeCreate() {
+    this.$store.commit('initializeStore')
+  },
+  mounted() {
+      document.title = 'My Account';
+      this.getUser();
+  },
   methods: {
     logout() {
       axios.defaults.headers.common["Authorization"] = ""
@@ -32,6 +50,31 @@ export default {
       this.$store.commit('removeToken')
 
       this.$router.push('/')
+    },
+    async getAdminUrl() {
+
+
+    },
+    async getUser() {
+      this.$store.commit('setIsLoading', true)
+      await axios
+          .get('/api/v1/users/me')
+          .then((response => {
+            this.user_id = response.data.id
+          }))
+          .catch((error => {
+            console.log(error)
+          }))
+      await axios
+          .get('/api/v1/user/' + this.user_id)
+          .then((response => {
+            this.user = response.data
+          }))
+          .catch((error => {
+            console.log(error)
+          }))
+      this.$store.commit('setIsLoading', false)
+
     }
   }
 }
