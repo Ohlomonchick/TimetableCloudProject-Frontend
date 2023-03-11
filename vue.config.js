98760cf71598d3b57c8007830f948523dbb48f81
Ohlomonchick <proskurin.dima16@gmail.com>
2023-03-11 18:45:28 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    disableHostCheck: true
-  }
+  devServer: { allowedHosts: 'all', },
 })
